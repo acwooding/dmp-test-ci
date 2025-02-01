19cf1f65949e1ca401bdff052f87becafd109766
Amy Wooding <36967030+acwooding@users.noreply.github.com>
2025-02-01 11:55:07 -0500

    original commit

---
+++ datamapplot/interactive_tests/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from '@playwright/test';
+import path from 'path';
+
+const htmlDir = path.resolve(__dirname, '../tests/html');
+const projDir = path.resolve(__dirname, '../..');
+
+console.log(`Resolved htmlDir: ${htmlDir}`);
+console.log(`Resolved htmlDir: ${projDir}`);
+
+export default defineConfig({
+  use: {
+    viewport: { width: 1280, height: 720 },
+    screenshot: 'on',
+    headless: true,
+  },
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixels: 100,
+      threshold: 0.2,
+      animations: 'disabled',
+    }
+  },
+  webServer: {
+    command: `python -m http.server 8000 -d ${htmlDir}`,
+    url: 'http://localhost:8000',
+    reuseExistingServer: true,
+    timeout: 120000
+  },
+  workers: process.env.CI ? 1 : undefined, // Run tests sequentially in CI
+  reporter: [['junit', { outputFile: `${projDir}/test-results/e2e-junit-results.xml` }]],
+});
